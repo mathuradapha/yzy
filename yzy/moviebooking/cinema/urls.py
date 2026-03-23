681f1e3f866f06da611db41a97a0d4e0351bb9b3
zeniiyaa <mathurada.pha@spumail.net>
2026-03-23 19:10:12 +0700

    Add files via upload

---
+++ yzy/moviebooking/cinema/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+
+path('', views.home),
+
+path('movie/<int:id>/', views.movie_detail, name='movie_detail'),
+
+path('seat/<int:id>/', views.seat, name='seat'),
+
+path('payment/<int:id>/', views.payment),
+
+path('ticket/<int:id>/',views.ticket),
+
+path('mytickets/',views.mytickets),
+
+
+
+# admin panel
+
+path('admin-dashboard/', views.admin_dashboard),
+
+path('add-movie/', views.add_movie),
+
+path('edit-movie/<int:id>/', views.edit_movie),
+
+path('delete-movie/<int:id>/', views.delete_movie),
+
+path('login/', views.login_user),
+
+path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+
+]
