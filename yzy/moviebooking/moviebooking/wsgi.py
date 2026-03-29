d7d8948a2aad6d6a6e122a92540ffe56dc98944a
zeniiyaa <mathurada.pha@spumail.net>
2026-03-29 16:15:52 +0700

    Update wsgi.py

--- yzy/moviebooking/moviebooking/wsgi.py
+++ yzy/moviebooking/moviebooking/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviebooking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yzy.moviebooking.moviebooking.settings')
 
 application = get_wsgi_application()
