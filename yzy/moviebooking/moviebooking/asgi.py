f11e69545f94d1812ee508d2337088fbe26e7ba0
zeniiyaa <mathurada.pha@spumail.net>
2026-03-29 16:55:28 +0700

    Update asgi.py

--- yzy/moviebooking/moviebooking/asgi.py
+++ yzy/moviebooking/moviebooking/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yzy.moviebooking.moviebooking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviebooking.settings')
 
 application = get_asgi_application()
